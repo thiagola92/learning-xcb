0636074bcb5798ee8ff2765c95a45806fa4bbfab
thiagola92 <thiagola92@gmail.com>
2025-01-14 04:40:38 -0300

    Add others draws

--- example07/main.c
+++ example07/main.c
@@ -27,30 +27,24 @@ int main() {
   xcb_map_window(connection, window_id);
   xcb_flush(connection);
 
-  // Create graphical context and set to draw to foreground.
   xcb_gcontext_t gcontext_id = xcb_generate_id(connection);
   uint32_t gcontext_mask = XCB_GC_FOREGROUND | XCB_GC_GRAPHICS_EXPOSURES;
   uint32_t gcontext_values[] = {screen->black_pixel, 0};
 
-  // NOTE: drawable type is "xcb_drawable_t" but we pass a "xcb_window_t".
   xcb_create_gc(connection, gcontext_id, screen->root, gcontext_mask,
                 gcontext_values);
 
-  // Prepare points do be draw.
-  int points_len = 16;
-  xcb_point_t points[] = {{10, 30}, {11, 30}, {12, 30}, {13, 30},
-                          {14, 30}, {15, 30}, {16, 30}, {17, 30},
-                          {18, 30}, {19, 30}, {20, 30}, {21, 30},
-                          {22, 30}, {23, 30}, {24, 30}, {25, 30}};
+  // Prepare points.
+  int points_len = 3;
+  xcb_point_t points[] = {{10, 30}, {10, 60}, {30, 60}};
 
   xcb_generic_event_t *event;
   while ((event = xcb_wait_for_event(connection))) {
     switch (event->response_type & ~0x80) {
     case XCB_EXPOSE:
-      // Draw points.
-      // NOTE: drawable type is "xcb_drawable_t" but we pass a "xcb_window_t".
-      xcb_poly_point(connection, XCB_COORD_MODE_ORIGIN, window_id, gcontext_id,
-                     points_len, points);
+      // Draw lines between consecutive points.
+      xcb_poly_line(connection, XCB_COORD_MODE_ORIGIN, window_id, gcontext_id,
+                    points_len, points);
 
       xcb_flush(connection);
       break;
